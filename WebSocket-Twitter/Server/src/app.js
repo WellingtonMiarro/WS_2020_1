4ecd4bd40ec35ae740100c62b53c2da20acf549f
WellingtonMiarro <whellingtonmiarroferreira@gmail.com>
2020-05-28 20:08:39 -0400

    Avaliação - Tweets

--- WebSocket-Twitter/Server/src/app.js
+++ WebSocket-Twitter/Server/src/app.js
@@ -1,58 +1,29 @@
 const http = require('http');
-const express = require('express')
 const server = http.createServer();
-
 const io = require('socket.io')(server);
 
-const Twit = require('twit');
+io.on('connection', socket => {
+    
+    var Twitter = require('node-tweet-stream')
+        socket = new Twitter({
+      consumer_key: '', // cole sua chave Aqui 
+      consumer_secret: '', // cole sua chave Aqui 
+      token: '', // cole sua chave Aqui 
+      token_secret: '' // cole sua chave Aqui 
+    })
 
-var T = new Twit({
-    consumer_key: 'ClkspMmRMOTAJvysssMpylY56', // Copie/cole sua chave aqui 
-    consumer_secret: '34qxajeFKcPTvMocYT5xMdOTZrQohbOAO1btgpRihJuA1tGxlM', // Copie/cole sua chave aqui
-    access_token: '138086873-jMTpMLwOTPKRRDQ9MnlgUvwIollZTadG3i9ZnRlz', // Copie/cole sua chave aqui
-    access_token_secret: 'xFdxY1bV2aRaxvy5AJ16i7kCxsVNe6MzThTr5QgJxJzwJ', // Copie/cole sua chave aqui
-    timeout_ms: 60 * 1000,  //tempo limite opcional da solicitação HTTP para aplicar a todas as solicitações.
-    strictSSL: true,     // opcional - requer que os certificados SSL sejam válidos.
+    socket.track('socket.io');
+    socket.track('Telegram'); // o Tweets
+
+    socket.on('tweet', function (tweet) { 
+        io.emit('tweet', tweet);
+        console.log('tweet received', tweet);
+      })
+       
+      socket.on('error', function (err) {
+        console.log('Error', err)
+      })
 });
-let stream = T.stream('statuses/filter', { track: 'Maisa' }); //A palavra ou Teg do Twitter entra Aqui!
-
-
-function getTweetObject(tweet) {
-  let tweetText = (tweet.extended_tweet) ? tweet.extended_tweet.full_text : tweet.text;
-
-  // verifique se há retweets
-  if (tweet.text.includes('RT @') && tweet.retweeted_status) {
-      tweetText = (tweet.retweeted_status.extended_tweet) ? tweet.retweeted_status.extended_tweet.full_text : tweet.retweeted_status.text;
-  }
-
-  let TweetObject = {
-      text: tweetText,
-      user: tweet.user.name,
-      location: (tweet.user.location !== null) ? tweet.user.location : '',
-      followers: tweet.user.followers_count,
-      userImage: tweet.user.profile_image_url,
-      timestamp: tweet.timestamp_ms,
-  };
-
-  return TweetObject;
-}
-let tweet = [];
-io.on('connection', socket=> {
-    socket.emit('mostrarTweet', tweet);
-
-  socket.on('tweet', () => {
-      stream.on('novoTweet', mostrarTweet => {
-        tweet.push(mostrarTweet)
-        socket.broadcast.emit('mostrarTweet', tweet);
-
-          let TweetObject = getTweetObject(novoTweet);
-
-          socket.emit('mostrarTweet', TweetObject);
-
-      });   
-  });
-});
-
-   
 
 module.exports = server;
+
